eb4b2ab31ee5abf98abd4adb8fe6afec1a810988
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-02-07 20:05:16 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+superbowlWin = (array) =>  {
+
+    const results = array.find((array) => array.results === "W")
+    return !!results ? array.year : undefined;
+
+}
+
+superbowlWin = (record) => {
+    const result = record.find((record) => record.result === "W");
+  
+    return !!result ? result.year : undefined;
+  };
+
